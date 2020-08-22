f599350fd09d5f8a7c245323ba635c2bcc45b056
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2020-08-22 16:26:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ const userRouter = require('./routes/users');
 app.use('/exercises',exerciseRouter);
 app.use('/users',userRouter);
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('../client/build/'));
+    app.use(express.static('client/build/'));
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname+'../client/build/index.html'));
+        res.sendFile(path.join(__dirname+'client/build/index.html'));
     });
 }
 
